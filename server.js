e147e1c9e87ec074593c19adbe08294f4c4e666d
apo-mak <apo-mak@users.noreply.github.com>
2021-06-27 12:52:02 +0300

    a

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 const API = require('static-api-generator')
 const api = new API({
-  blueprint: 'Data_source/:manufacture/:socket/:model/:variant',
+  blueprint: 'data_source/:manufacture/:socket/:model/:variant',
   targetDirectory: 'output'
 })
 
-api.generate({
-  endpoints: ['manufacture','variant']
-})
 
-
-api.generate({
-  endpoints: ['socket'],
-  levels: ['model', 'variant'],
-  root: 'manufacture'
-})
+// api.generate({
+//   endpoints: ['socket'],
+//   levels: ['model', 'variant'],
+//   root: 'manufacture'
+// })
 
 api.generate({
   endpoints: ['mb_codes']
